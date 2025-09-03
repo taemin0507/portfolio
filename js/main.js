098bce1d599403e20c066753fa3a1bea42841ad3
taemin0507 <xoals28655@gmail.com>
2025-09-03 15:01:40 +0900

    up

--- js/main.js
+++ js/main.js
@@ -85,3 +85,81 @@ spyEls.forEach(function (spyEl) {
 console.log(new Date().getFullYear());
 const thisYear = document.querySelector('.this-year');
 thisYear.textContent = new Date().getFullYear();
+
+const levelEL = document.querySelector('.hero-card');
+const level = document.querySelector('.level');
+const note = document.querySelector('.note');
+levelEL.addEventListener('click', function () {
+if (level.textContent === 'Lv.0 → Lv.1') {
+    level.textContent = 'Lv.1 → Lv.2';
+    note.textContent = '"아직은 시작에 불과해!"';
+  } else if (level.textContent === 'Lv.1 → Lv.2') {
+    level.textContent = 'Lv.2 → Lv.3';
+    note.textContent = '"오늘도 활기차게!"';
+  } else if (level.textContent === 'Lv.2 → Lv.3') {
+    level.textContent = 'Lv.3 → Lv.4';
+    note.textContent = '"너의 가능성은 무궁무진해!"';
+  } else if (level.textContent === 'Lv.3 → Lv.4') {
+    level.textContent = 'Lv.4 → Lv.5';
+    note.textContent = '"이제 조금은 개발자 다워졌는걸!"';
+  } else if (level.textContent === 'Lv.4 → Lv.5') {
+    level.textContent = 'Lv.5 → Lv.6';
+    note.textContent = '"벌써 6레벨이라니!"';
+  } else if (level.textContent === 'Lv.5 → Lv.6') {
+    level.textContent = 'Lv.6 → Lv.7';
+    note.textContent = '"조금만 더 힘내!"';
+  } else if (level.textContent === 'Lv.6 → Lv.7') {
+    level.textContent = 'Lv.7 → Lv.8';
+    note.textContent = '"개발자로써 1인분은 할 수 있게 됐어!"';
+  } else if (level.textContent === 'Lv.7 → Lv.8') {
+    level.textContent = 'Lv.8 → Lv.9';
+    note.textContent = '"거의 정상이야!"';
+  } else if (level.textContent === 'Lv.8 → Lv.9') {
+    level.textContent = 'Lv.9 → Lv.MAX';
+    note.textContent = '"개발자가 된걸 축하해!"';
+  } else if (level.textContent === 'Lv.9 → Lv.MAX') {
+    level.textContent = 'Lv.0 → Lv.1';
+    note.textContent = '"기초부터 탄탄히!"';
+  }
+});
+
+const dog = document.querySelector('.dog');
+
+dog.addEventListener("mouseenter", () => {
+  // blur 효과 적용
+  dog.style.filter = "blur(5px)";
+
+  setTimeout(() => {
+    dog.src = './images/fish.jpg';
+    dog.style.filter = "";
+  }, 1000);
+});
+
+dog.addEventListener("mouseleave", () => {
+
+  dog.style.filter = "blur(5px)";
+
+   setTimeout(() => {
+  dog.src = './images/pro.jpg';
+  dog.style.filter = ""; 
+    }, 1000);
+});
+
+const btnHamburger = document.querySelector('.btn-hamburger');
+const navEl = document.querySelector('header nav');
+const aEl = document.querySelectorAll('header nav ul li a');
+
+btnHamburger.addEventListener('click',function(){
+  // if (!navEl.classList.contains('active')){
+  //   navEl.classList.add('active');
+  // }else{
+  //   navEl.classList.remove('active');
+  // }
+  navEl.classList.toggle('active');
+});
+
+aEl.forEach(function(a){
+  a.addEventListener('click',function(){
+    navEl.classList.remove('active');
+  });
+});
